f291810b45c53229c9c47bd52f825a88b7afd2af
David Colle <david.colle@fabernovel.com>
2017-12-29 12:49:53 +0000

    Merged in feature/23-vfs_configuration_by_uri (pull request #30)
    23 - VFS configuration by URI
    
    * renamed/moved S3URIProvider to core URIProvider
    
    * first draft
    
    * minor scaladoc fixes
    
    * added explicit typing in URIProvider
    
    * first draft
    
    * used getSchemeSpecificPart instead of drop(n)
    
    * renammed some tests
    
    Approved-by: Cedric Chantepie <c.chantepie@yahoo.fr>

--- google/src/test/scala/GoogleTransportSpec.scala
+++ google/src/test/scala/GoogleTransportSpec.scala
@@ -12,47 +12,47 @@ import scala.util.Failure
 class GoogleTransportSpec extends Specification {
   "GoogleTransport" title
 
-  "GoogleTransport.apply using URIs" should {
+  "Factory using URI" should {
     val filename = "gcs-test.json"
     val projectId = TestUtils.config.getString("google.storage.projectId")
     val application = s"benji-tests-${System identityHashCode this}"
 
     import TestUtils.WS
 
-    "returns Failure when the provider fail" in {
+    "return Failure when the provider fail" in {
       val exception: Throwable = new Exception("foo")
       implicit val provider = URIProvider[Throwable](Failure[URI])
 
       GoogleTransport(exception) must beFailedTry.withThrowable[Exception]("foo")
     }
 
-    "returns Failure when given a null URI" in {
+    "return Failure when given a null URI" in {
       GoogleTransport(null: URI) must beFailedTry.withThrowable[IllegalArgumentException]
     }
 
-    "returns Success when given a proper uri as String" in {
+    "return Success when given a proper uri as String" in {
       GoogleTransport(s"google:classpath://$filename?application=$application&projectId=$projectId") must beSuccessfulTry
     }
 
-    "returns Success when given a proper uri as URI" in {
+    "return Success when given a proper uri as URI" in {
       val uri = new URI(s"google:classpath://$filename?application=$application&projectId=$projectId")
 
       GoogleTransport(uri) must beSuccessfulTry
     }
 
-    "returns Failure with wrong scheme" in {
+    "return Failure with wrong scheme" in {
       GoogleTransport(s"google:wrong://$filename?application=$application&projectId=$projectId") must beFailedTry.withThrowable[MalformedURLException]
     }
 
-    "returns Failure without scheme prefix" in {
+    "return Failure without scheme prefix" in {
       GoogleTransport(s"classpath://$filename?application=$application&projectId=$projectId") must beFailedTry.withThrowable[IllegalArgumentException]
     }
 
-    "returns Failure when given a uri without application parameter" in {
+    "return Failure when given a uri without application parameter" in {
       GoogleTransport(s"google:classpath://$filename?projectId=$projectId") must beFailedTry.withThrowable[IllegalArgumentException]
     }
 
-    "returns Failure when given a uri without projectId parameter" in {
+    "return Failure when given a uri without projectId parameter" in {
       GoogleTransport(s"google:classpath://$filename?application=$application") must beFailedTry.withThrowable[IllegalArgumentException]
     }
   }
---
+++ vfs/src/test/scala/VFSTransportSpec.scala
@@ -0,0 +1,46 @@
+package tests.benji.vfs
+
+import java.net.URI
+
+import org.specs2.mutable.Specification
+
+import com.zengularity.benji.vfs.VFSTransport
+import com.zengularity.benji.URIProvider
+
+import scala.util.Failure
+
+class VFSTransportSpec extends Specification {
+  "VFSTransport" title
+
+  "Factory using URs" should {
+    "return Failure when the provider fail" in {
+      val exception: Throwable = new Exception("foo")
+      implicit val provider = URIProvider[Throwable](Failure[URI])
+
+      VFSTransport(exception) must beFailedTry.withThrowable[Exception]("foo")
+    }
+
+    "return Failure when given a null URI" in {
+      VFSTransport(null: URI) must beFailedTry.withThrowable[IllegalArgumentException]
+    }
+
+    "return Success when given a proper uri as String" in {
+      VFSTransport("vfs:file:///home/someuser/somedir") must beSuccessfulTry
+    }
+
+    "return Success when given a proper uri as URI" in {
+      val uri = new URI("vfs:file:///home/someuser/somedir")
+
+      VFSTransport(uri) must beSuccessfulTry
+    }
+
+    "return Failure with wrong scheme" in {
+      VFSTransport("vfs:wrong://path") must beFailedTry.withThrowable[IllegalArgumentException]
+    }
+
+    "return Failure without scheme prefix" in {
+      VFSTransport("file:///home/someuser/somedir") must beFailedTry.withThrowable[IllegalArgumentException]
+    }
+  }
+
+}
