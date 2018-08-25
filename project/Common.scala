72b725fa2d6d852951bfd0a52d008f059b973d46
Cédric Chantepie <cchantep@users.noreply.github.com>
2018-08-25 15:18:07 +0200

    Task/mima (#20)
    Setup MiMa

--- project/Common.scala
+++ project/Common.scala
@@ -29,7 +29,8 @@ object Common {
       "-Ywarn-nullary-override",
       "-Ywarn-nullary-unit",
       "-g:vars"),
-    javacOptions in (Compile, compile) ++= Seq("-target", "1.8"),
+    javacOptions in (Compile, compile) ++= Seq(
+      "-source", "1.8", "-target", "1.8"),
     scalacOptions in (Compile, console) ~= {
       _.filterNot { opt => opt.startsWith("-X") || opt.startsWith("-Y") }
     },
