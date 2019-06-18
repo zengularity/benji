4a6e9d70e5dc6f588827a1a2a3de075242ef8b61
nicolasdejonghe <nicolas.dejonghe@fabernovel.com>
2019-06-18 11:53:48 +0200

    refacto

--- gridfs/src/main/scala/GridFSTransport.scala
+++ gridfs/src/main/scala/GridFSTransport.scala
@@ -1,17 +1,16 @@
-// /*
-//  * Copyright (C) 2018-2019 Zengularity SA (FaberNovel Technologies) <https://www.zengularity.com>
-//  */
+/*
+ * Copyright (C) 2018-2019 Zengularity SA (FaberNovel Technologies) <https://www.zengularity.com>
+ */
 package com.zengularity.benji.gridfs
 
-import reactivemongo.api.MongoConnection
-
-import reactivemongo.api.gridfs.GridFS
-import reactivemongo.api.BSONSerializationPack
 import scala.concurrent.{ ExecutionContext, Future }
 import scala.util.{ Failure, Success, Try }
 
+import reactivemongo.api.MongoConnection
+import reactivemongo.api.gridfs.GridFS
+import reactivemongo.api.BSONSerializationPack
+
 import com.zengularity.benji.exception.BenjiUnknownError
-import scala.util.Success
 
 final class GridFSTransport(driver: reactivemongo.api.MongoDriver, connection: MongoConnection, mongoUri: MongoConnection.ParsedURI) {
   def close(): Unit = {
@@ -21,29 +20,26 @@ final class GridFSTransport(driver: reactivemongo.api.MongoDriver, connection: M
   def gridfs(prefix: String)(implicit ec: ExecutionContext): Future[GridFS[BSONSerializationPack.type]] = {
     mongoUri.db match {
       case Some(name) =>
-        for {
-          db <- connection.database(name)
-          gridfs = GridFS[BSONSerializationPack.type](db, prefix)
-        } yield gridfs
+        connection.database(name).map(db => GridFS[BSONSerializationPack.type](db, prefix))
 
       case None =>
-        Future.failed(new BenjiUnknownError(s"Couldn't get the db from $mongoUri"))
+        Future.failed(new BenjiUnknownError(s"Fails to get the db from $mongoUri"))
     }
   }
 }
 
 object GridFSTransport {
-  def apply(uri: String)(implicit ec: ExecutionContext): Try[GridFSTransport] = {
+  def apply(uri: String): Try[GridFSTransport] = {
     val driver = new reactivemongo.api.MongoDriver
 
     val res = for {
       mongoUri <- MongoConnection.parseURI(uri)
-      con = driver.connection(mongoUri)
-    } yield (mongoUri, con)
-    val (mongoUri, connection) = (res.map(_._1), res.map(_._2))
-    connection match {
-      case Success(connection) => new Success(GridFSTransport(driver, connection, mongoUri))
-      case Failure(_) => Failure[GridFSTransport](new BenjiUnknownError(s"Couldn't create the connection to $uri"))
+      con <- driver.connection(mongoUri, strictUri = true)
+    } yield (con, mongoUri)
+
+    res match {
+      case Success((connection, mongoUri)) => Success(new GridFSTransport(driver, connection, mongoUri))
+      case Failure(error) => Failure[GridFSTransport](new BenjiUnknownError(s"Fails to create the connection to $uri", Some(error)))
     }
   }
 }
