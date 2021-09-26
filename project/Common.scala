e75c4a5fabf8bf7a94e26dd508f0685b0b59e35c
Cédric Chantepie <cchantep@users.noreply.github.com>
2021-09-26 14:24:13 +0200

    Close #291 - Update silencer-lib, silencer-plugin to 1.7.6

--- project/Common.scala
+++ project/Common.scala
@@ -55,7 +55,7 @@ object Common extends AutoPlugin {
     },
     resolvers += Resolver.sonatypeRepo("staging" /* releases */ ),
     libraryDependencies ++= {
-      val silencerVer = "1.7.5"
+      val silencerVer = "1.7.6"
 
       Seq(
         compilerPlugin(("com.github.ghik" %% "silencer-plugin" % silencerVer).
