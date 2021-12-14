36b1f5a4a5d730ac67dfa73d4713ebb7a04046ae
Cédric Chantepie <cchantep@users.noreply.github.com>
2021-12-14 10:51:20 +0100

    Update build (#327)
    * Close #325 - Update sbt-scalafix to 0.9.33
    * Close #326 - Update sbt to 1.5.6

--- .ci_scripts/validate.sh
+++ .ci_scripts/validate.sh
@@ -29,8 +29,16 @@ echo "$GOOGLE_CREDENTIALS" | base64 -d | gzip -dc > \
   google/src/test/resources/gcs-test.json
 
 # Runtime settings
-JVM_MAX_MEM="2048M"
-JVM_OPTS="-Xms$JVM_MAX_MEM -Xmx$JVM_MAX_MEM -XX:+CMSClassUnloadingEnabled -XX:ReservedCodeCacheSize=192m -XX:MetaspaceSize=512M -XX:MaxMetaspaceSize=512M"
+JVM_MAX_MEM="1G"
+
+JAVA_MAJOR=`java -version 2>&1 | head -n 1 | cut -d '"' -f 2 | sed -e 's/\.[0-9a-zA-Z_]*$//'`
+
+if [ "v$JAVA_MAJOR" = "v10.0" -o "v$JAVA_MAJOR" = "v11.0" ]; then
+  JVM_OPTS="-XX:+UseContainerSupport -XX:MaxRAMPercentage=70"
+else
+  JVM_MAX_MEM="1632M" # 1760M"
+  JVM_OPTS="-Xmx$JVM_MAX_MEM -XX:ReservedCodeCacheSize=192m"
+fi
 
 export _JAVA_OPTIONS="$JVM_OPTS"
 
