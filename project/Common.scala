186f991cb228e19891afffb553f33855911e6ea1
Cédric Chantepie <cchantep@users.noreply.github.com>
2024-07-22 10:29:33 +0200

    Close #471 - Update logback-classic to 1.2.13

--- project/Common.scala
+++ project/Common.scala
@@ -118,7 +118,7 @@ object Common extends AutoPlugin {
     }) ++ Seq(
       "com.typesafe.akka" %% "akka-stream-testkit" % akkaVer.value,
       "com.typesafe.akka" %% "akka-slf4j" % akkaVer.value,
-      "ch.qos.logback" % "logback-classic" % "1.2.12"
+      "ch.qos.logback" % "logback-classic" % "1.2.13"
     ).map(_ % Test),
     Compile / compile / javacOptions ++= Seq(
       "-source",
